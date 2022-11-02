8978cc5edc204fb43a2ca0aaa71f454d14bdaaa4
jnoms <jasonlnomburg@gmail.com>
2022-11-02 11:32:14 -0700

    Revert "Handle when there are no results."
    This reverts commit 7f6783f2b59470a46d804f79d29ea050c8b8f473.

--- bin/bash/hhblits.sh
+++ bin/bash/hhblits.sh
@@ -118,9 +118,7 @@ hhblits \
     -cpu $THREADS
 
 echo "$0: Filtering the blasttab output by evalue"
-# The sed removes empty lines
-awk -v EVALUE=$EVALUE '$11 < EVALUE' ${BLAST_OUT}.tmp |\
-    sed -i '/^$/d' >> \
-    ${BLAST_OUT} && rm ${BLAST_OUT}.tmp
+awk -v EVALUE=$EVALUE '$11 < EVALUE' ${BLAST_OUT}.tmp >> ${BLAST_OUT} && \
+    rm ${BLAST_OUT}.tmp
 
 echo "$0: Finished at $(date)"
