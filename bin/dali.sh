3cdb1a6e7a0a2a87c18517b8ef061e124e942078
jnoms <jasonlnomburg@gmail.com>
2023-03-23 15:54:16 -0700

    git hell

--- bin/dali.sh
+++ bin/dali.sh
@@ -30,13 +30,6 @@ usage() {
             is the most important information about the alignments. transrot is 
             highly useful for rotating target structures after alignment. The additional
             field is alignments.
-        -Q --SINGLE_QUERY {string} ['']
-            This should be the basename of a single .dat file. If this is specified, 
-            the query_list will be just that single basename, and that will be the only
-            query. This is a helpful option if you want to submit many parallel jobs and
-            want to search for just one query at a time per script execution. Note that 
-            the entiry should be exactly what the dat file is named, e.g. 
-            'xxxxA.dat' or something like that.
 
         Some additional details:
         - Because DALI hates when paths are above 60 or 80 characters, the query_dir and 
